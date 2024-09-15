484c33f130ac7ab276d93a862df28a0bb6f2a377
Romerico <mrh.jrd.234@gmail.com>
2024-09-15 06:30:29 -0400

    removed files

--- recipe-scraping-project/src/shared/models/recipe-model.ts
+++
@@ -1,8 +0,0 @@
-export interface RecipeData {
-    _id?: string;
-    name?: string;
-    ingredients?: string[];
-    instructions?: string[];
-    nutritionInfo?: string[];
-    image?: string;
-}
